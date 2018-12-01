f6a166d4baa674ec8de71696072437a07abd2026
woosung.shin <sws7420@gmail.com>
2018-12-02 00:04:40 +0900

    yarn add react router and set router

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
+import Router from "Components/Router";
 
 function App() {
-  return (
-    <div className="App" />
-  );
+  return <><Router /></>;
 }
 
 export default App;
