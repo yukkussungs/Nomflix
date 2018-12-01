6596c8fd48dce696b3129ca12a5ce538d48b321b
woosung.shin <sws7420@gmail.com>
2018-12-02 02:02:40 +0900

    set styled-componentes and set header nav

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Router from "Components/Router";
 
 function App() {
-  return <><Router /></>;
+  return <>
+    <Router />
+  </>;
 }
 
 export default App;
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "Routes/Home";
 import Detail from "Routes/Detail";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
+import Header from "Components/Header";
 
 export default () => (
     <Router>
+        <Header />
         <Route path="/" exact component={Home} />
         <Route path="/detail" exact component={Detail} />
         <Route path="/search" exact component={Search} />
