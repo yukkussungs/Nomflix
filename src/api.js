f156ab4d628e47751ac6167fde6ea0374d79593d
woosung.shin <sws7420@gmail.com>
2018-12-04 00:49:37 +0900

    set movie api and create Home Container and Presenter components

--- src/api.js
+++ src/api.js
@@ -8,4 +8,38 @@ const api = axios.create({
   }
 });
 
-export default api;
+export const moviesApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upComing: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popluar"),
+  movieDetail: id =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_resonse: "videos"
+      }
+    }),
+  search: term =>
+    api.get("search/movie", {
+      params: {
+        query: encodeURIComponent(term)
+      }
+    })
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/top_rated"),
+  airingToday: () => api.get("tv/airing_today"),
+  showDetail: id =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_resonse: "videos"
+      }
+    }),
+  search: term =>
+    api.get("search/tv", {
+      params: {
+        query: encodeURIComponent(term)
+      }
+    })
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from 'Components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "Components/App";
+import "./api.js";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
