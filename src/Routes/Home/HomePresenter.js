63ef70068a329de6f1448a501e73282e3d88422e
woosung.shin <sws7420@gmail.com>
2018-12-05 01:23:27 +0900

    create Section components and write homepresenter

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,1 +1,40 @@
-export default () => "Home";
+import React from "react";
+import styled from "styled-components";
+import Section from "Components/Section";
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
+
+const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) => {
+  loading ? null : (
+    <Container>
+      {/* children props */}
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map(movie => movie.title)}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming Movies">
+          {upcoming.map(movie => movie.title)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Movies">
+          {popular.map(movie => movie.title)}
+        </Section>
+      )}
+    </Container>
+  );
+};
+
+HomePresenter.propTypes = {
+  nowPlaying: propTypes.array,
+  popular: PropTypes.array,
+  upcoming: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string
+};
+
+export default HomePresenter;
